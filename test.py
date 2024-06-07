d0a4b41cc5c7b9fd62f611be9e55479548c9de74
Siddhartha-Kolla <siddharthansg@gmail.com>
2024-06-07 16:57:22 +0200

    s

--- test.py
+++ test.py
@@ -1,43 +1,6 @@
-from itertools import combinations
-
-def distribute_pairs(combinations, num_players):
-    rounds = []
-    assigned_players = set()
-
-    for pair in combinations:
-        # Check if any player in the pair is already assigned to a pair in the existing rounds
-        if not any(player in assigned_players for player in pair):
-            # Add the pair to the current round
-            rounds.append(pair)
-            # Mark both players in the pair as assigned
-            assigned_players.update(pair)
-
-            # If all players are assigned, start a new round
-            if len(assigned_players) == num_players:
-                assigned_players = set()
-                yield rounds  # Yield the current round
-                rounds = []  # Start a new round
-
-def max_rounds_possible(combinations, num_players):
-    max_rounds = 0
-    remaining_combinations = combinations.copy()
-
-    while remaining_combinations:
-        # Distribute pairs for one round
-        for round_num, round_combinations in enumerate(distribute_pairs(remaining_combinations, num_players), 1):
-            print(f"Round {round_num}: {round_combinations}")
-            max_rounds += 1
-
-        # Remove distributed pairs from the list of combinations
-        for pair in round_combinations:
-            remaining_combinations.remove(pair)
-
-    return max_rounds
-
-# Example usage
-lcombinations = list(combinations(range(1,40+1),2))
-print(lcombinations)
-print()
-num_players = 40
-max_rounds = max_rounds_possible(lcombinations, num_players)
-print("Maximum rounds possible:", max_rounds)
+l = [1,2,3,4,5,6,7,8,9,10]
+turned_list = l[1:]
+print(l)
+turned_list_final = [l[0]]
+turned_list_final.extend(turned_list[-1:] + turned_list[:-1])
+print(turned_list_final)
